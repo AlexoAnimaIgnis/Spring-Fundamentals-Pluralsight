2517a6d9312245130f14cfc9f707ca0fcecd07b5
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2021-07-25 05:06:22 +0800

    Singleton bean scope

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,13 +2,16 @@ import com.pluralsight.repository.HibernateSpeakerRepository;
 import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
 
     @Bean(name = "speakerService")
+    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
     public SpeakerService getSpeakerService() {
         SpeakerServiceImpl speakerService = new SpeakerServiceImpl();
         speakerService.setSpeakerRepository(getSpeakerRepository());
--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,6 +8,11 @@ public class Application {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
         SpeakerService service = applicationContext.getBean("speakerService", SpeakerService.class);
+        System.out.println(service);
         System.out.println(service.findAll().get(0).getFirstName());
+
+        SpeakerService service2 = applicationContext.getBean("speakerService", SpeakerService.class);
+        System.out.println(service2);
+        System.out.println(service2.findAll().get(0).getFirstName());
     }
 }
