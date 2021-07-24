fbde5c64c4ca3b408ceb856cf231e666ec870018
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2021-07-25 05:07:36 +0800

    Prototype bean scope

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Scope;
 public class AppConfig {
 
     @Bean(name = "speakerService")
-    @Scope(value = BeanDefinition.SCOPE_SINGLETON)
+    @Scope(value = BeanDefinition.SCOPE_PROTOTYPE)
     public SpeakerService getSpeakerService() {
         SpeakerServiceImpl speakerService = new SpeakerServiceImpl();
         speakerService.setSpeakerRepository(getSpeakerRepository());
